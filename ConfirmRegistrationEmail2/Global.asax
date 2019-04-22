e1a281f30bd52093c54588419ea0457d448f7bb1
Ілля Слівчук <illyasliva433456@ukr.net>
2019-04-22 19:28:40 +0300

    Add project files.

---
+++ ConfirmRegistrationEmail2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ConfirmRegistrationEmail2.MvcApplication" Language="C#" %>
---
+++ ConfirmRegistrationEmail2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ConfirmRegistrationEmail2.Startup))]
+namespace ConfirmRegistrationEmail2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
